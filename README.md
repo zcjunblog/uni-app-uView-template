524f32de2793ad320aa1c4ad90dfefad99819016
zhaozc <i@zcjun.com>
2020-06-18 15:15:29 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# uniapp
-dt-uniapp
+# dt-uniapp专用模板
+# 使用
+
+## 1.安装项目模板
+
+`` vue create -p dcloudio/uni-preset-vue 项目名称
+
+## 2.选择自定义模板
+
+![使用自定义模板](http://yanxuan.nosdn.127.net/2a8f418e2e5633228a739a58eef77cd7.png)
+
+## 3.填入仓库地址
+
+模板地址即仓库地址:
+
+![模板地址](http://yanxuan.nosdn.127.net/46ea1808bbeff69a0f9fe601b878287c.png)
+
+![填入仓库地址](http://yanxuan.nosdn.127.net/f2322eb9f87d15eb41afc2bfcb4dffca.png)
+
+## 4.安装所需插件
+
+```npm i sass-loader ```
+
+```
+node-sass被墙解决方法:
+npm i node-sass --sass_binary_site=https://npm.taobao.org/mirrors/node-sass/
+```
+
+## 5.运行、发布uni-app
+
+``npm run dev:%PLATFORM%``
+
+`` npm run build:%PLATFORM%``
+
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <van-icon name="thumb-circle-o" /> 创建完毕
+        <div style="margin-top: 180rpx;text-align: center"><van-icon name="thumb-circle-o" size="70" /></div>
+        <div style="font-size: 80rpx;text-align: center">{{msg}}</div>
     </div>
 </template>
 
@@ -13,7 +14,9 @@
 
         },
         data() {
-
+            return{
+                msg:'创建完毕'
+            }
         },
         computed: {
             ...mapState({
@@ -38,147 +41,4 @@
 
 <style lang="scss" scoped>
     @import '@/static/style/reference.scss';
-
-    .page-main {
-        background-color: $page-bg;
-        min-height: 100vh;
-        padding-bottom: 2vw;
-        box-sizing: border-box;
-    }
-
-    $slide-height: 340rpx;
-
-    .swiper-main {
-        height: $slide-height;
-        overflow: hidden;
-        position: relative;
-        margin-bottom: 10rpx;
-
-        .swiper {
-            height: $slide-height;
-        }
-
-        .slide-image {
-            height: 100%;
-            width: 100%;
-            display: block;
-        }
-
-        .user-info-bar {
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            width: 100%;
-            height: 60rpx;
-            line-height: 60rpx;
-            background-color: rgba(0, 0, 0, .3);
-            text-indent: 26rpx;
-            color: #fff;
-            font-size: 28rpx;
-        }
-    }
-
-    .tab-main {
-        margin-top: 10rpx;
-
-    }
-    .tab-item{
-
-    }
-    .statis-card {
-        width: 91.5vw;
-        display: flex;
-        align-items: center;
-        margin-bottom: 10rpx;
-        margin-right: 2.7vw;
-        margin-left: 2.7vw;
-        background: #fff;
-        height: 80rpx;
-        line-height: 80rpx;
-        padding: 0 1.7vw;
-        /*box-shadow: 0 3px 23px 0 rgba(174, 56, 56, .08);*/
-        border-radius: 1vw;
-    }
-
-    .statis-card .statis-title {
-        font-size: $font-size;
-        font-weight: 600;
-        color: #333;
-        margin-left: 6vw;
-        position: relative;
-        display: inline-block;
-    }
-
-    .statis-card .statis-title:before {
-        content: "";
-        width: 10rpx;
-        background: #cd2c1f;
-        left: -5vw;
-        border-radius: 6rpx;
-        position: absolute;
-        top: 2.5vw;
-        bottom: 2.5vw;
-    }
-
-    .starDetai {
-        display: inline-block;
-        /*font-size: 6vw;*/
-        text-align: center;
-        width: 55%;
-    }
-
-    .starScore {
-        color: $theme;
-        display: inline-block;
-        padding-left: 1vw;
-
-        span {
-            font-size: 3vw;
-        }
-    }
-
-    .tab-header {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 94.6vw;
-        height: 80rpx;
-        background: #fff;
-        text-align: center;
-        margin: auto;
-        border-radius: 1vw;
-        font-size: $font-size;
-
-        .tab-title {
-            flex: 1;
-            height: 100%;
-            border-radius: 1vw;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: relative;
-        }
-    }
-
-    .tab-active {
-        background: $theme-dark;
-        color: #fff;
-    }
-
-    .tag-icon {
-        position: absolute;
-        right: 0.8vw;
-        top: 0.8vw;
-        width: 3vw;
-        height: 3vw;
-        background-color: $red;
-        border-radius: 50%;
-        font-size: 2.3vw;
-        color: #fff;
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 0.6vw;
-    }
 </style>
